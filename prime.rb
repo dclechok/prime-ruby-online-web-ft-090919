4eea2d7cbf1d485066e05371824c0fa14d877bd3
Daniel Lechok <dclechok@gmail.com>
2019-09-11 16:40:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+def prime?(integer)
+	c = 1
+	full_int_array = []
+  while c <= integer
+		full_int_array << c
+		c += 1
+	end
+
+	full_int_array.any? do |num|
+		if integer % num == 0
+			return false
+		end
+	end
+end
+
+
+
+# A prime number is a natural number greater than 1
+
+# only factors is 1, and the number we're testing
+
+#divides by a positive integer and still produces a positive integer
