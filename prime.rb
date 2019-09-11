012c8e33c063e404303da08e62fe013323ed1a28
Daniel Lechok <dclechok@gmail.com>
2019-09-11 18:23:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,17 +2,22 @@ def prime?(integer)
 	c = 1
 	full_int_array = []
   while c <= integer
-		full_int_array << c
+		full_int_array << c.to_f
 		c += 1
 	end
 
-	full_int_array.any? do |num|
-		if integer % num == 0
+	full_int_array.shift
+	full_int_array.pop
+	d = 0
+	while d < full_int_array.length
+		if(integer <= 0 || integer % full_int_array[d] == 0)
 			return false
+			d += 1
+		else
+			return true
 		end
 	end
 end
-
 
 
 # A prime number is a natural number greater than 1
